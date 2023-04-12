ae66339380ba95c5651b78686c32db91bb85c519
jeremylin0603 <90104158+jeremylin0603@users.noreply.github.com>
2023-04-12 21:29:53 +0800

    fix: 移除沒意義的變數引用

--- test04_1-Debounce.js
+++ test04_1-Debounce.js
@@ -2,14 +2,10 @@ export const debounce = (func, delay) => {
     let timeTicker = null
 
     return function () {
-        // 在外部執行環境保存變數和 this 指向
-        const args = arguments
-        const _this = this
-
-        clearTimeout(timeTicker)
+        if (timeTicker) clearTimeout(timeTicker)
 
         timeTicker = setTimeout(() => {
-            func.apply(_this, args)
+            func.apply(this, arguments)
         }, delay);
     }
 }
